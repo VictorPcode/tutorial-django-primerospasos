3dda628fec2b0b787c0fa01385ce843daff34fb2
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2022-11-15 17:28:34 -0300

    vistas de add que toma los valores nombre y apellido del formulario y los redirije a la vista en la tabla de members

--- jirestore/members/views.py
+++ jirestore/members/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponse
 from django.template import loader
+from django.urls import reverse
+from .models import Members
 
 def index(request):
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
+        
     template = loader.get_template('landing.html')
-    return HttpResponse(template.render())
+    context = {
+        'mymembers': mymembers,
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('addmb.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
